66cb4658cf595aea427e8d3ef8ff3796da6c1af4
Catatomik <cata@catadev.org>
2024-07-24 15:04:11 +0200

    feat(front): remove link

--- pages/links.vue
+++ pages/links.vue
@@ -9,7 +9,7 @@
 
     </div>
     <div class="flex w-full flex-col" v-else>
-      <LinkCard :link="link" v-for="link in links" @edited="fetchLinks()"" />
+      <LinkCard :link="link" v-for="link in links" @edited="fetchLinks()" @removed="fetchLinks()" />
     </div>
   </div>
 </template>
